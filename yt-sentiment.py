fccb433f80520ee026bd9714aef94e71490dbc94
Alex Ritt <46687522+AR0106@users.noreply.github.com>
2021-11-17 22:46:36 -0600

    level 3

--- yt-sentiment.py
+++ yt-sentiment.py
@@ -1,57 +1,83 @@
+import time
 import nltk
+import requests
+from bs4 import BeautifulSoup
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from rake_nltk import Rake
+from selenium import webdriver
 
 # Input
-print('Please Enter The Sentence You Would Like to Analyze:')
-sentence = input()
+print('Please Enter The URL of the Video You Would Like to Analyze:')
+url = input()
 
-# Sentiment Analysis
-try:
+
+driver = webdriver.Chrome()
+
+driver.get('https://www.youtube.com/watch?v=A9D_VShUbCo&t=23s')  # example video
+
+time.sleep(2)  # if you have slow internet , increase the value of sleep
+
+
+driver.execute_script('window.scrollTo(1, 500);')
+# Here is a trick , Youtube only load comments when you scroll just down of video , if you scroll bottom or elsewhere, comments will not load , so first scroll to that down part and wait for loading comments after that scroll to bottom or whenever you want
+# now wait let load the comments
+time.sleep(5)
+
+driver.execute_script('window.scrollTo(1, 3000);')
+
+
+comment_div = driver.find_element_by_xpath('//*[@id="contents"]')
+comments = comment_div.find_elements_by_xpath('//*[@id="content-text"]')
+for comment in comments:
+    print(comment.text)
+
+    # Sentiment Analysis
+    try:
+        sentimentAnalyzer = SentimentIntensityAnalyzer()
+    except LookupError:
+        nltk.download('vader_lexicon')
+
     sentimentAnalyzer = SentimentIntensityAnalyzer()
-except LookupError:
-    nltk.download('vader_lexicon')
 
-sentimentAnalyzer = SentimentIntensityAnalyzer()
+    scores = sentimentAnalyzer.polarity_scores(comment.text)
 
-scores = sentimentAnalyzer.polarity_scores(sentence)
+    if scores['neg'] > scores['pos'] and scores['neg'] > scores['neu']:
+        print('Negative')
+    elif scores['neg'] < scores['pos'] and scores['pos'] > scores['neu']:
+        print('Positive')
+    else:
+        print('Neutral')
 
-if scores['neg'] > scores['pos'] and scores['neg'] > scores['neu']:
-    print('Negative')
-elif scores['neg'] < scores['pos'] and scores['pos'] > scores['neu']:
-    print('Positive')
-else:
-    print('Neutral')
+    # Keyword Extraction
+    try:
+        rake = Rake()
+    except LookupError:
+        nltk.download('stopwords')
+        nltk.download('punkt')
 
-# Keyword Extraction
-try:
     rake = Rake()
-except LookupError:
-    nltk.download('stopwords')
-    nltk.download('punkt')
 
-rake = Rake()
+    try:
+        rake.extract_keywords_from_text(comment.text)
+    except LookupError:
+        nltk.download('punkt')
 
-try:
-    rake.extract_keywords_from_text(sentence)
-except LookupError:
-    nltk.download('punkt')
+    rake.extract_keywords_from_text(comment.text)
 
-rake.extract_keywords_from_text(sentence)
+    keywords = rake.get_ranked_phrases()
 
-keywords = rake.get_ranked_phrases()
+    print('Keywords:')
+    print(keywords)
 
-print('Keywords:')
-print(keywords)
+    # Safety Features
+    flags = ['check', 'channel', 'subscribe', 'website', 'give', 'me']
 
-# Safety Features
-flags = ['check', 'channel', 'subscribe', 'website', 'give', 'me']
+    flagCount = 0
 
-flagCount = 0
+    for keyword in keywords:
+        if keyword in flags:
+            flagCount += 1
 
-for keyword in keywords:
-    if keyword in flags:
-        flagCount += 1
-
-if flagCount >= 2:
-    print('! Possible Spam !')
+    if flagCount >= 2:
+        print('! Possible Spam !')
+    print('------------------------------------------------')
