9223796042c957f1b3af66e5a88533b6ae497de6
Alex Ritt <46687522+AR0106@users.noreply.github.com>
2021-11-17 21:26:07 -0600

    level 2

--- yt-sentiment.py
+++ yt-sentiment.py
@@ -1,17 +1,18 @@
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from rake_nltk import Rake
 
+# Input
+print('Please Enter The Sentence You Would Like to Analyze:')
+sentence = input()
+
+# Sentiment Analysis
 try:
     sentimentAnalyzer = SentimentIntensityAnalyzer()
 except LookupError:
     nltk.download('vader_lexicon')
 
-
 sentimentAnalyzer = SentimentIntensityAnalyzer()
-
-print('Please Enter The Sentence You Would Like to Analyze:')
-sentence = input()
-
 
 scores = sentimentAnalyzer.polarity_scores(sentence)
 
@@ -21,3 +22,36 @@ elif scores['neg'] < scores['pos'] and scores['pos'] > scores['neu']:
     print('Positive')
 else:
     print('Neutral')
+
+# Keyword Extraction
+try:
+    rake = Rake()
+except LookupError:
+    nltk.download('stopwords')
+    nltk.download('punkt')
+
+rake = Rake()
+
+try:
+    rake.extract_keywords_from_text(sentence)
+except LookupError:
+    nltk.download('punkt')
+
+rake.extract_keywords_from_text(sentence)
+
+keywords = rake.get_ranked_phrases()
+
+print('Keywords:')
+print(keywords)
+
+# Safety Features
+flags = ['check', 'channel', 'subscribe', 'website', 'give', 'me']
+
+flagCount = 0
+
+for keyword in keywords:
+    if keyword in flags:
+        flagCount += 1
+
+if flagCount >= 2:
+    print('! Possible Spam !')
